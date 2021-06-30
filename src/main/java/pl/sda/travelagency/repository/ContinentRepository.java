96a5d24d3d60bcf855875abb9af0dfb80b0613d5
KrzysztofStasiak <krzystof@kursGIT.pl>
2021-06-30 16:13:48 +0200

    Dodanie encji Continent, Country, City, Hotel oraz podstawowe repository i service do nich

---
+++ src/main/java/pl/sda/travelagency/repository/ContinentRepository.java
@@ -0,0 +1,9 @@
+package pl.sda.travelagency.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.travelagency.model.ContinentEntity;
+
+@Repository
+public interface ContinentRepository extends JpaRepository<ContinentEntity,Long> {
+}
