15c2489b1962b9ebee93e8e3289268045663e91a
Sylwiajustynska <sylwia.justynska@gmail.com>
2021-07-14 20:26:11 +0200

    Dodawanie użytkownika do rejestracji

--- src/main/java/pl/sda/travelagency/controller/LoginRegister.java
+++ src/main/java/pl/sda/travelagency/controller/LoginRegister.java
@@ -1,12 +1,17 @@
 package pl.sda.travelagency.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.sda.travelagency.model.UserEntity;
+import pl.sda.travelagency.repository.UserEntityRepository;
 
-@Controller
+@Controller @RequiredArgsConstructor
 public class LoginRegister {
-
+private final UserEntityRepository userEntityRepository;
     @RequestMapping(value = "/login",method = { RequestMethod.GET, RequestMethod.POST })
     public String getLogin(){
         return "LoginPages/login";
@@ -17,7 +22,9 @@ public class LoginRegister {
     }
 
     @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public String register(){
+    public String register(@ModelAttribute(name = "user") UserEntity user,
+                           Model model){
+        userEntityRepository.save(user);
         return "LoginPages/login";
     }
 }
