32aa4aafbe1870e15fd52014c267e52d30d3632b
KrzysztofStasiak <krzystof@kursGIT.pl>
2021-07-14 18:39:51 +0200

    Dodanie widoku i metod edycji wycieczki oraz usuniecia. Poprawa nazewnictwa w serwisach encji.

--- src/main/java/pl/sda/travelagency/service/CityService.java
+++ src/main/java/pl/sda/travelagency/service/CityService.java
@@ -1,32 +1,30 @@
-package pl.sda.travelagency.Service;
+package pl.sda.travelagency.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.sda.travelagency.model.CityEntity;
 import pl.sda.travelagency.repository.CityRepository;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Service
 public class CityService {
     private final CityRepository cityRepository;
 
-    public CityService(CityRepository cityRepository) {
-        this.cityRepository = cityRepository;
-    }
 
-    public List<CityEntity> listAllCities() {
+    public List<CityEntity> listAll() {
         return cityRepository.findAll();
     }
 
-    public void saveCity(CityEntity city) {
+    public void save(CityEntity city) {
         cityRepository.save(city);
     }
 
-    public CityEntity getCityById(Long id) {
+    public CityEntity getById(Long id) {
         return cityRepository.findById(id).get();
     }
 
-    public void deleteCity(Long id) {
+    public void delete(Long id) {
         cityRepository.deleteById(id);
     }
 }
